4c5b0d831dedb4ec874033f5eb89dec9eeb55b38
rswinkle <rswinkle@dedd6328-c50f-0410-8be5-b54a49638459>
2016-08-22 21:20:07 +0000

    Organizational changes
    Merge many of the changes from header_madness branch, ie
    
    Delete util.c, newfunc.c and merge contents into misc.c
    
    Delete proto.h, append to header.h
    
    Delete init.c, add to globals.c
    
    add globals.h and separate out parser_types.h from parser.h to make them
    compile and enable future organizational improvements
    
    Delete dead code from func.h etc.  Sure there's much more dead code that
    I deleted in header_madness that I need to repeat here.
    
    Edit Makefile.am and run automake due to removed files.
    
    
    
    git-svn-id: http://oss.mars.asu.edu/svn/davinci/davinci/trunk@19518 dedd6328-c50f-0410-8be5-b54a49638459

--- globals.c
+++ globals.c
@@ -1,4 +1,4 @@
-#include "parser.h"
+#include "parser_types.h"
 #include <setjmp.h>
 #include <stdio.h>
 
@@ -29,157 +29,70 @@ int indent   = 0;
 
 Var* curnode = NULL;
 
-void quit(int);
-
-void make_sym(Var* v, int format, char* str)
-{
-	V_FORMAT(v)  = format;
-	V_DSIZE(v)   = 1;
-	V_SIZE(v)[0] = V_SIZE(v)[1] = V_SIZE(v)[2] = 1;
-	V_ORG(v)                                   = BSQ;
-	V_DATA(v)                                  = calloc(1, NBYTES(format));
-
-	switch (format) {
-	case DV_INT32: *((int*)(V_DATA(v))) = strtol(str, NULL, 10); break;
-	case DV_FLOAT: {
-		double d;
-		d = strtod(str, NULL);
-		if (((d > FLT_MAX) || (d < FLT_MIN)) && (d != 0)) {
-			free(V_DATA(v));
-			V_DATA(v)               = calloc(1, NBYTES(DV_DOUBLE));
-			V_FORMAT(v)             = DV_DOUBLE;
-			*((double*)(V_DATA(v))) = d;
-		} else {
-			*((float*)(V_DATA(v))) = d;
-		}
-	}
-	}
-}
 
 /*
-** This is similar to parse_buffer, but it doesn't print the stack
-** or clean up the scope
+const char* FORMAT2STR[] = {
+    0,           "byte",    "uint16", "uint32", "uint64",
+
+    "int8",
+    "short", // really int16
+    "int",   // really int32
+    "int64",
+
+    "float",     "double",
+
+    "vax float", "vax int",
+};
 */
-Var* eval_buffer(char* buf)
-{
-	int i, j;
-	extern char* yytext;
-	Var* v = NULL;
-	void* parent_buffer;
-	void* buffer;
-	Var* node;
 
-	extern void* get_current_buffer();
-	extern void* yy_scan_string();
-	extern void yy_delete_buffer(void*);
-	extern void yy_switch_to_buffer(void*);
 
-	parent_buffer = (void*)get_current_buffer();
-	buffer        = (void*)yy_scan_string(buf);
+#define NUM_TYPE_STRS 13
 
-	while ((i = yylex()) != 0) {
-		/*
-		** if this is a function definition, do no further parsing yet.
-		*/
-		j = yyparse(i, (Var*)yytext);
-		if (j == -1) quit(0);
+const char* FORMAT_STRINGS[NUM_TYPE_STRS] = {
+	"byte",
+	"short",
+	"int",
 
-		if (j == 1 && curnode != NULL) {
-			node = curnode;
-			evaluate(node);
-			/* // v = pop(scope_tos()); */
-			free_tree(node);
-		}
-	}
+	"uint8",
+	"uint16",
+	"uint32",
+	"uint64",
 
-	yy_delete_buffer((struct yy_buffer_state*)buffer);
-	if (parent_buffer) yy_switch_to_buffer(parent_buffer);
-	return (v);
-}
+	"int8",
+	"int16",
+	"int32",
+	"int64",
 
-void quit(int return_code)
-{
-	char* path = getenv("TMPDIR");
+	"float",
+	"double"
 
-	if (interactive) {
-		printf("\n");
-#if defined(USE_X11_EVENTS) && defined(HAVE_LIBREADLINE)
-		/* JAS FIX */
-		rl_callback_handler_remove();
-#endif
-	}
+//	,"vax float", "vax int"
+};
 
-	clean_scope(scope_tos());
+const int STR_TO_FORMAT[NUM_TYPE_STRS] = {
+	DV_UINT8,
+	DV_INT16,
+	DV_INT32,
 
-	/**
-	 ** clean up temporary directory
-	 **/
-	rmrf(path);
-	exit(return_code);
-}
+	DV_UINT8,
+	DV_UINT16,
+	DV_UINT32,
+	DV_UINT64,
 
-void yyerror(char* s)
-{
-	extern int pp_count;
-	extern int pp_line;
+	DV_INT8,
+	DV_INT16,
+	DV_INT32,
+	DV_INT64,
 
-	printf("***%*s^ ", pp_count, " ");
-	printf("%s, line %d\n", s, pp_line);
-}
+	DV_FLOAT,
+	DV_DOUBLE
 
-int yywrap()
-{
-	return (1);
-}
+//	,VAX_FLOAT,
+//	VAX_INTEGER
+};
 
-char* unquote(char* name)
-{
-	char* p = name;
+#undef NUM_TYPE_STRS
 
-	if (name == NULL) return (NULL);
-	if (*name == 0) return (name);
-	if (*name == '"') {
-		p++;
-		name++;
-		while (*name) {
-			if (*name == '"' && *(name - 1) != '\\') break;
-			name++;
-		}
-		*name = '\0';
-	} else if (*name == '\'') {
-		p++;
-		name++;
-		while (*name) {
-			if (*name == '\'' && *(name - 1) != '\\') break;
-			name++;
-		}
-		*name = '\0';
-	}
-	return (p);
-}
+const char* ORG2STR[] = {"bsq", "bil", "bip"};
 
-char* unescape(char* str)
-{
-	char* p = str;
-	char* q = str;
 
-	if (str && *str) {
-		while (*p) {
-			if (*p == '\\') {
-				if (*(p + 1) == 't')
-					*q = '\t';
-				else if (*(p + 1) == 'n')
-					*q = '\n';
-				else
-					*q = *(p + 1);
-				p++;
-			} else {
-				*q = *p;
-			}
-			p++;
-			q++;
-		}
-		*q = *p;
-	}
-	return (str);
-}
